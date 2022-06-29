9b683ebb78821a35a5898ea4e08d29d5be012ec1
VjVijay1505 <vjk.vijaykmr@gmail.com>
2022-06-29 13:45:50 +0530

    added allowed host requirements.txt

--- demo/settings.py
+++ demo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-i+ezn(-xva+1w!)^j^cwuo)!n%=r@k!&l9fxeeo2yi2gmn@kgk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['revise2.azurewebsites.net','127.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
